a25e2f38b90962b33d39315388942b647b5c1e6a
t125083 <t125083@ed.sus.ac.jp>
2026-01-26 12:09:29 +0900

    feat/app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-db = pd.read_csv()
+db1 = pd.read_csv('consumption.csv')
+db2 = pd.read_csv('production.csv')
 
 st.title('日本の食料自給率')
