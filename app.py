5e202fb16a3704ed50f8220d6a4ab1ac7a5459d0
t125083 <t125083@ed.sus.ac.jp>
2026-02-02 07:16:04 +0900

    feat/fin

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ db = pd.read_csv('production.csv')
 
 st.title("日本の食料生産量")
 st.subheader('-日本国内の生産量を、1961年から2024年までのデータから種類別に調べることができます。-')
-st.caption('画面左のサイドバーで条件指定')
+st.caption('画面左で条件指定')
 
+col = st.columns(2)
 kinds = [i for i in db.columns if i != "kind"] #kindを除く1列目の要素をリストにする
 db['年'] = db['kind'] #年の1961~2024の列をdbに追加する
 
-with st.sidebar:
+with col[0].container(border=True):
     st.subheader('調べる種類')
     kind = st.selectbox('選択してください', kinds)
     st.subheader('グラフの種類')
@@ -24,8 +25,14 @@ if chart == "折れ線":
 elif chart == "棒グラフ":
     fig = px.bar(fdb, x="年", y=kind)
 
-st.text(f"現在選択中  --{kind}--")
-st.plotly_chart(fig)
+with col[1].container():
+    st.text(f"現在選択中  --{kind}--")
+    st.plotly_chart(fig)
 
-st.text("すべてのデータ")
-st.bar_chart(db, x="年")
+activated = st.toggle("すべてのデータ")
+if activated == True:
+    st.bar_chart(db, x="年")
+    st.table(db)
+
+st.subheader("グラフからわかること")
+st.text('食料生産量は品目別にみても1980年頃～2000年頃がピークであり、それ以降は年々生産量は落ちてきている。')
